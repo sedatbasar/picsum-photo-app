5c235a646f1fe7c18d2e999c444ca98ab0f097a8
sedatbasar <sedat@whitespectre.com>
2025-03-13 23:59:37 +0300

    Move pagination into imagelist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-
 import ImageList from '@/components/ImageList';
-import { Pagination } from '@/components/Pagination';
 
-export default function Home() {
-  const searchParams = useSearchParams();
-  const currentPage = parseInt(searchParams.get('page') || '1', 10);
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const awaitedSearchParams = await searchParams;
+  const currentPage = parseInt(awaitedSearchParams.page || '1', 10);
 
   return (
     <main className="p-4 container mx-auto">
       <h1 className="text-2xl font-bold mb-4">Picsum Photo Gallery</h1>
       <ImageList currentPage={currentPage} />
-      <Pagination currentPage={currentPage} totalPages={10} />
     </main>
   );
 }
--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -7,6 +7,7 @@ import { getImages } from '@/api/get-images';
 import { Card, CardContent } from '@/components/ui/card';
 
 import { Skeleton } from './ui/skeleton';
+import { Pagination } from './Pagination';
 
 export const IMAGES_PER_PAGE = 12;
 
@@ -45,27 +46,31 @@ export default function ImageList({ currentPage }: { currentPage: number }) {
   }
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-      {images.map((image) => (
-        <Card
-          key={image.id}
-          className="shadow-lg hover:scale-105 transition-transform duration-200"
-        >
-          <CardContent className="p-2 h-48 relative">
-            <div className="relative w-full h-full rounded overflow-hidden">
-              <Image
-                src={image.download_url}
-                alt={`Image by ${image.author}`}
-                fill
-                className="rounded cursor-pointer object-cover inset-0 w-full h-full"
-              />
-            </div>
-            <p className="absolute bottom-2 left-2 text-sm text-muted-foreground bg-white bg-opacity-50 p-1 rounded">
-              By {image.author}
-            </p>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        {images.map((image) => (
+          <Card
+            key={image.id}
+            className="shadow-lg hover:scale-105 transition-transform duration-200"
+          >
+            <CardContent className="p-2 h-48 relative">
+              <div className="relative w-full h-full rounded overflow-hidden">
+                <Image
+                  src={image.download_url}
+                  alt={`Image by ${image.author}`}
+                  fill
+                  sizes="(max-width: 768px) 50vw, 25vw"
+                  className="rounded cursor-pointer object-cover inset-0 w-full h-full"
+                />
+              </div>
+              <p className="absolute bottom-2 left-2 text-sm text-muted-foreground bg-white bg-opacity-50 p-1 rounded">
+                By {image.author}
+              </p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <Pagination currentPage={currentPage} totalPages={10} />
+    </>
   );
 }
